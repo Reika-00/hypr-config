090ac6e7a242b4b2a886950e24925db31af40ea0
Reika-00 <kerbit156@gmail.com>
2025-06-13 10:44:14 +0300

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,8 @@
 fastfetch --kitty-direct /home/rei/.config/fastfetch/flag_small.png
 
-
 # Commands to run in interactive sessions can go here
 
-# Отключаем приветственное сообщение fish
 set -U fish_greeting ''
 
-
-
-if test -f ~/.cache/wal/colors.fish
-    source ~/.cache/wal/colors.fish
-end
 
 
