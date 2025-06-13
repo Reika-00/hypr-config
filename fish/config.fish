a2b5f60262206a95e99c5dba093b63c4eb22fc2b
Reika-00 <kerbit156@gmail.com>
2025-06-13 10:43:04 +0300

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+fastfetch --kitty-direct /home/rei/.config/fastfetch/flag_small.png
+
+
+# Commands to run in interactive sessions can go here
+
+# Отключаем приветственное сообщение fish
+set -U fish_greeting ''
+
+
+
+if test -f ~/.cache/wal/colors.fish
+    source ~/.cache/wal/colors.fish
+end
+
+
